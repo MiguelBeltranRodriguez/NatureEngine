f8760750a95b0f1fac1ff03a756c8aaa3e36037b
Miguel Angel <miguelito_goz360@hotmail.com>
2019-08-24 17:31:25 -0500

    Motor listo para dibujar rectangulos y ovalos

--- src/controlador/Renderizador2D.java
+++ src/controlador/Renderizador2D.java
@@ -1,5 +1,6 @@
 package controlador;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferStrategy;
 
@@ -23,7 +24,9 @@ public class Renderizador2D {
 			return;
 		}
 		g = (Graphics2D) bs.getDrawGraphics();
+		g.clearRect(0, 0, VarGlobalVista.widhtPantalla, VarGlobalVista.heightPantalla);
 		//empieza a dibujar
+		g.setColor(Color.white);
 		g.fillRect(0, 0, VarGlobalVista.widhtPantalla, VarGlobalVista.heightPantalla);
 		
 		
@@ -31,6 +34,15 @@ public class Renderizador2D {
 		
 		bs.show();
 		g.dispose();
+	}
+	
+	public void dibujarRectangulo(Color c, int x, int y, int width, int height) {
+		g.setColor(c);
+		g.fillRect(x, y, width, height);
+	}
+	public void dibujarOvalo(Color c, int x, int y, int width, int height) {
+		g.setColor(c);
+		g.fillOval(x, y, width, height);
 	}
 
 }
---
+++ src/modelo/Dibujable.java
@@ -0,0 +1,7 @@
+package modelo;
+
+import java.awt.Graphics2D;
+
+public interface Dibujable {
+	public void dibujar(Graphics2D g);
+}
