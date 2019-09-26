d6648022f14f8319f5b29e5776d5f375658a5bfa
Miguel Angel <miguelito_goz360@hotmail.com>
2019-09-26 14:50:23 -0500

    indentacion

--- PrimeraEntrega/NatureEngineCommons/src/main/java/NatureEngine/NatureEngineCommons/ObjetoDistribuido.java
+++ PrimeraEntrega/NatureEngineCommons/src/main/java/NatureEngine/NatureEngineCommons/ObjetoDistribuido.java
@@ -5,18 +5,18 @@ import java.rmi.RemoteException;
 
 
 public abstract class ObjetoDistribuido implements Serializable{
-	
 
-		/**
-	 * 
-	 */
+
+
+
 	private static final long serialVersionUID = 1L;
-		protected Long ID;
-		
-		public ObjetoDistribuido(Long ID) throws RemoteException {
-			super();
-			this.ID = ID;
-		}
+	protected Long ID;
 
-			
+
+	public ObjetoDistribuido(Long ID) throws RemoteException {
+		super();
+		this.ID = ID;
+	}
+
+
 }
